ec5ada5415d62a29fa43ba928572087afdad1ccf
steve donovan <steve.j.donovan@gmail.com>
2013-04-18 15:32:21 +0200

    global alias detection starting to work...

---
+++ tests/alias.lua
@@ -0,0 +1,7 @@
+local io,table,math = io,table,math
+
+print(io.print)
+
+local function boo()
+  return io.print
+end
---
+++ tests/lhs.lua
@@ -0,0 +1,2 @@
+foo = tonumber(arg[1])
+print = function(s) io.write(s,'\n') end
